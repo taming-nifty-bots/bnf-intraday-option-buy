0ff99a1f6f6de23d1b634b6bd75a60220bb741cf
sugam gupta <sugam192@gmail.com>
2026-05-10 00:24:51 +0530

    Add PCR conditions for bullish and bearish positions

--- bnf-option-buy/option_buy.py
+++ bnf-option-buy/option_buy.py
@@ -479,10 +479,10 @@ def main():
                             return
 
                 elif strategies.count_documents({'entry_date': str(datetime.datetime.now().date())}) < max_trades:
-                    if get_color() == 'green' and get_instrument_close() > get_high40() and current_time < datetime.time(hour=15, minute=5) and get_close_time() > datetime.datetime.now().replace(hour=9, minute=20, second=0, microsecond=0) and get_close_time() > get_last_exit_time() and get_rsi() > 60:
+                    if get_color() == 'green' and get_instrument_close() > get_high40() and current_time < datetime.time(hour=15, minute=5) and get_close_time() > datetime.datetime.now().replace(hour=9, minute=20, second=0, microsecond=0) and get_close_time() > get_last_exit_time() and get_rsi() > 60 and get_pcr() < 0.8:
                         print("Creating Bullish Position")
                         buy_call()
-                    elif get_color() == 'red' and get_instrument_close() < get_low40() and current_time < datetime.time(hour=15, minute=5) and get_close_time() > datetime.datetime.now().replace(hour=9, minute=20, second=0, microsecond=0) and get_close_time() > get_last_exit_time() and get_rsi() < 40:
+                    elif get_color() == 'red' and get_instrument_close() < get_low40() and current_time < datetime.time(hour=15, minute=5) and get_close_time() > datetime.datetime.now().replace(hour=9, minute=20, second=0, microsecond=0) and get_close_time() > get_last_exit_time() and get_rsi() < 40 and get_pcr() > 1.2:
                         print("Creating Bearish Position")
                         buy_put()
                     else:
